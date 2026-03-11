f0f153f3f32f3188598aa3eedac65f87b24b1e05
Marko <shutka132@gmail.com>
2026-03-11 22:50:13 +0100

    initial commit

---
+++ src/main/java/com/e_feesh/EFeeshApplication.java
@@ -0,0 +1,13 @@
+package com.e_feesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFeeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFeeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_feesh/EFeeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_feesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFeeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
