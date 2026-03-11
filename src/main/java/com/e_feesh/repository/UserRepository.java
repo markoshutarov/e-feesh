db212a6fea38b258bd2fa4e1f2bd06d7118fbeed
Marko <shutka132@gmail.com>
2026-03-11 23:11:12 +0100

    Added User repo + UserDetails service

---
+++ src/main/java/com/e_feesh/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.e_feesh.repository;
+
+import com.e_feesh.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUsername(String username);
+}
